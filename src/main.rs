4d2a751a5c3ddad0067ca4b1be9951866894a707
MightyAlex200 <quantumtraveling@gmail.com>
2019-10-26 03:19:10 -0400

    Better structure but still very much unfinished

--- src/main.rs
+++ src/main.rs
@@ -14,88 +14,204 @@ use std::{
 mod types;
 mod xml;
 
-use types::{Color3uint8, CoordinateFrame, Item, Vector3};
+use types::{CFrame, Color3, Item, Property, Vector3};
 use xml::*;
 
+const SCALE: f32 = 2.;
+
 lazy_static! {
-	static ref BRICK_SIZES: HashMap<String, types::size> =
-		FromIterator::from_iter(vec![].into_iter());
-	static ref BRICK_SIZE_REGEX: Regex = Regex::new(r"(\d+?)x(\d+)(F| Base)?").unwrap();
+	static ref REGULAR_BRICK_RE: Regex = Regex::new(r"(\d+?)x(\d+)(F| Base)?").unwrap();
 }
 
 // CFrame but only the rotation matrix values
-struct CFrameQuat {
-	r00: f64,
-	r01: f64,
-	r02: f64,
-	r10: f64,
-	r11: f64,
-	r12: f64,
-	r20: f64,
-	r21: f64,
-	r22: f64,
+// struct CFrameQuat {
+// 	r00: f64,
+// 	r01: f64,
+// 	r02: f64,
+// 	r10: f64,
+// 	r11: f64,
+// 	r12: f64,
+// 	r20: f64,
+// 	r21: f64,
+// 	r22: f64,
+// }
+
+// const ROTATIONS: [CFrameQuat; 4] = [
+// 	CFrameQuat {
+// 		r00: 1.,
+// 		r01: 0.,
+// 		r02: 0.,
+// 		r10: 0.,
+// 		r11: 1.,
+// 		r12: 0.,
+// 		r20: 0.,
+// 		r21: 0.,
+// 		r22: 1.,
+// 	},
+// 	CFrameQuat {
+// 		r00: 0.,
+// 		r01: 0.,
+// 		r02: 1.,
+// 		r10: 0.,
+// 		r11: 1.,
+// 		r12: 0.,
+// 		r20: -1.,
+// 		r21: 0.,
+// 		r22: 0.,
+// 	},
+// 	CFrameQuat {
+// 		r00: -1.,
+// 		r01: 0.,
+// 		r02: 0.,
+// 		r10: 0.,
+// 		r11: 1.,
+// 		r12: 0.,
+// 		r20: 0.,
+// 		r21: 0.,
+// 		r22: -1.,
+// 	},
+// 	CFrameQuat {
+// 		r00: 0.,
+// 		r01: 0.,
+// 		r02: -1.,
+// 		r10: 0.,
+// 		r11: 1.,
+// 		r12: 0.,
+// 		r20: 1.,
+// 		r21: 0.,
+// 		r22: 0.,
+// 	},
+// ];
+
+// fn cframe_with_rot(x: f64, y: f64, z: f64, rotation: u8) -> CoordinateFrame {
+// 	let CFrameQuat {
+// 		r00,
+// 		r01,
+// 		r02,
+// 		r10,
+// 		r11,
+// 		r12,
+// 		r20,
+// 		r21,
+// 		r22,
+// 	} = ROTATIONS[rotation as usize];
+// 	CoordinateFrame {
+// 		x,
+// 		y,
+// 		z,
+// 		r00,
+// 		r01,
+// 		r02,
+// 		r10,
+// 		r11,
+// 		r12,
+// 		r20,
+// 		r21,
+// 		r22,
+// 	}
+// }
+
+// fn autobricksize(name: &str) -> Option<types::size> {
+// 	let captures = BRICK_SIZE_REGEX.captures(name)?;
+// 	let (x, z): (f64, f64) = (
+// 		captures.get(1)?.as_str().parse().ok()?,
+// 		captures.get(2)?.as_str().parse().ok()?,
+// 	);
+// 	if let Some(_) = captures.get(3) {
+// 		// F
+// 		Some(types::size(Vector3 {
+// 			x: x * 0.5,
+// 			y: 0.2,
+// 			z: z * 0.5,
+// 		}))
+// 	} else {
+// 		Some(types::size(Vector3 {
+// 			x: x * 0.5,
+// 			y: 0.6,
+// 			z: z * 0.5,
+// 		}))
+// 	}
+// }
+
+// fn get_brick_size(brick: &bl_save::BrickBase) -> types::size {
+// 	BRICK_SIZES
+// 		.get(&brick.ui_name)
+// 		.cloned()
+// 		.or_else(|| autobricksize(&brick.ui_name))
+// 		.unwrap_or_else(|| {
+// 			println!("UNKNOWN BRICK NAME: {}", brick.ui_name);
+// 			types::size(Vector3 {
+// 				x: 1.,
+// 				y: 1.,
+// 				z: 1.,
+// 			})
+// 		})
+// }
+
+// fn get_brick_cframe(brick: &bl_save::BrickBase) -> CFrame {
+// 	cframe_with_rot(
+// 		brick.position.0 as f64,
+// 		brick.position.2 as f64,
+// 		-brick.position.1 as f64,
+// 		brick.angle,
+// 	)
+// }
+
+fn items_from_brick(brick: bl_save::BrickBase, colors: &[(f32, f32, f32, f32); 64]) -> Vec<Item> {
+	match get_brick_type(&brick) {
+		BrickType::Regular { cframe, size } => vec![{
+			let mut item = Item::default("Part".to_string());
+			item.properties
+				.insert("size".to_string(), Property::Vector3(size));
+			item.properties
+				.insert("CFrame".to_string(), Property::CFrame(cframe));
+			item.properties.insert(
+				"Color3uint8".to_string(),
+				Property::Color3(colors[brick.color_index as usize].into()),
+			);
+			item
+		}],
+		BrickType::Wedge { cframe, size } => unimplemented!(),
+		BrickType::Unknown => {
+			println!("UNKNOWN BRICK: {:?}", brick);
+			vec![]
+		}
+	}
+	// Item::new(
+	// 	"Part",
+	// 	types::RBXUUID(uuid::Uuid::new_v4()),
+	// 	types::Name(brick.ui_name.to_string()),
+	// 	get_brick_cframe(&brick),
+	// 	get_brick_size(&brick),
+	// 	get_brick_color(&brick, colors),
+	// )
 }
 
-const ROTATIONS: [CFrameQuat; 4] = [
-	CFrameQuat {
-		r00: 1.,
-		r01: 0.,
-		r02: 0.,
-		r10: 0.,
-		r11: 1.,
-		r12: 0.,
-		r20: 0.,
-		r21: 0.,
-		r22: 1.,
-	},
-	CFrameQuat {
-		r00: 0.,
-		r01: 0.,
-		r02: 1.,
-		r10: 0.,
-		r11: 1.,
-		r12: 0.,
-		r20: -1.,
-		r21: 0.,
-		r22: 0.,
-	},
-	CFrameQuat {
-		r00: -1.,
-		r01: 0.,
-		r02: 0.,
-		r10: 0.,
-		r11: 1.,
-		r12: 0.,
-		r20: 0.,
-		r21: 0.,
-		r22: -1.,
-	},
-	CFrameQuat {
-		r00: 0.,
-		r01: 0.,
-		r02: -1.,
-		r10: 0.,
-		r11: 1.,
-		r12: 0.,
-		r20: 1.,
-		r21: 0.,
-		r22: 0.,
-	},
-];
+fn get_brick_type(brick: &bl_save::BrickBase) -> BrickType {
+	if let Some(caps) = REGULAR_BRICK_RE.captures(&brick.ui_name) {
+		let x: f32 = caps.get(1).unwrap().as_str().parse().unwrap(); // These will never panic, check the RE
+		let z: f32 = caps.get(2).unwrap().as_str().parse().unwrap();
+		let y = if let Some(_) = caps.get(3) { 0.4 } else { 1.2 };
+		BrickType::Regular {
+			size: Vector3::new(x * SCALE, y * SCALE, z * SCALE),
+			cframe: cframe_from_pos_and_rot(brick.position, brick.angle),
+		}
+	} else {
+		BrickType::Unknown // TODO: Wedge
+	}
+}
 
-fn cframe_with_rot(x: f64, y: f64, z: f64, rotation: u8) -> CoordinateFrame {
-	let CFrameQuat {
-		r00,
-		r01,
-		r02,
-		r10,
-		r11,
-		r12,
-		r20,
-		r21,
-		r22,
-	} = ROTATIONS[rotation as usize];
-	CoordinateFrame {
+fn cframe_from_pos_and_rot(pos: (f32, f32, f32), angle: u8) -> CFrame {
+	let x = pos.0 * 2. * SCALE;
+	let y = pos.2 * 2. * SCALE;
+	let z = -pos.1 * 2. * SCALE;
+	let (r00, r01, r02, r10, r11, r12, r20, r21, r22) = match angle {
+		0 => (1., 0., 0., 0., 1., 0., 0., 0., 1.),
+		1 => (0., 0., 1., 0., 1., 0., -1., 0., 0.),
+		2 => (-1., 0., 0., 0., 1., 0., 0., 0., -1.),
+		_ => (0., 0., -1., 0., 1., 0., 1., 0., 0.),
+	};
+	CFrame {
 		x,
 		y,
 		z,
@@ -111,71 +227,10 @@ fn cframe_with_rot(x: f64, y: f64, z: f64, rotation: u8) -> CoordinateFrame {
 	}
 }
 
-fn autobricksize(name: &str) -> Option<types::size> {
-	let captures = BRICK_SIZE_REGEX.captures(name)?;
-	let (x, z): (f64, f64) = (
-		captures.get(1)?.as_str().parse().ok()?,
-		captures.get(2)?.as_str().parse().ok()?,
-	);
-	if let Some(_) = captures.get(3) {
-		// F
-		Some(types::size(Vector3 {
-			x: x * 0.5,
-			y: 0.2,
-			z: z * 0.5,
-		}))
-	} else {
-		Some(types::size(Vector3 {
-			x: x * 0.5,
-			y: 0.6,
-			z: z * 0.5,
-		}))
-	}
-}
-
-fn get_brick_size(brick: &bl_save::BrickBase) -> types::size {
-	BRICK_SIZES
-		.get(&brick.ui_name)
-		.cloned()
-		.or_else(|| autobricksize(&brick.ui_name))
-		.unwrap_or_else(|| {
-			println!("UNKNOWN BRICK NAME: {}", brick.ui_name);
-			types::size(Vector3 {
-				x: 1.,
-				y: 1.,
-				z: 1.,
-			})
-		})
-}
-
-fn get_brick_cframe(brick: &bl_save::BrickBase) -> CoordinateFrame {
-	cframe_with_rot(
-		brick.position.0 as f64,
-		brick.position.2 as f64,
-		brick.position.1 as f64,
-		brick.angle,
-	)
-}
-
-fn get_brick_color(brick: &bl_save::BrickBase, colors: &[(f32, f32, f32, f32); 64]) -> Color3uint8 {
-	let color = colors[brick.color_index as usize];
-	Color3uint8 {
-		r: (color.0 * 255.) as u8,
-		g: (color.1 * 255.) as u8,
-		b: (color.2 * 255.) as u8,
-		a: (color.3 * 255.) as u8,
-	}
-}
-
-fn item_from_brick(brick: bl_save::BrickBase, colors: &[(f32, f32, f32, f32); 64]) -> Item {
-	Item::new(
-		"Part",
-		types::RBXUUID(uuid::Uuid::new_v4()),
-		types::Name(brick.ui_name.to_string()),
-		get_brick_cframe(&brick),
-		get_brick_size(&brick),
-		get_brick_color(&brick, colors),
-	)
+enum BrickType {
+	Regular { cframe: CFrame, size: Vector3 },
+	Wedge { cframe: CFrame, size: Vector3 },
+	Unknown,
 }
 
 fn main() {
@@ -188,7 +243,9 @@ fn main() {
 
 	for (i, brick) in reader.into_iter().enumerate() {
 		let brick = brick.unwrap();
-		items.push(item_from_brick(brick.base, &colors));
+		for item in items_from_brick(brick.base, &colors) {
+			items.push(item);
+		}
 		println!(
 			"{} bricks processed ({}%)",
 			i,
--- src/types.rs
+++ src/types.rs
@@ -1,387 +1,230 @@
+use std::collections::HashMap;
 use uuid::Uuid;
 
-pub trait Prop: ToString {}
-
-impl<T> Prop for T where T: ToString {}
-
-pub struct RBXUUID(pub Uuid);
-
-pub struct Item {
-    class: &'static str,
-    referent: RBXUUID,
-    properties: Vec<Box<dyn Prop>>,
-}
-
-impl ToString for Item {
-    fn to_string(&self) -> String {
-        format!(
-            "<Item class=\"{}\" referent=\"{}\">
-    <Properties>
-        {}
-    </Properties>
-</Item>",
-            self.class,
-            self.referent.0.to_simple_ref().to_string(),
-            self.properties
-                .iter()
-                .map(|p| p.to_string())
-                .collect::<Vec<_>>()
-                .join("\n")
-        )
-    }
-}
-
-impl Item {
-    pub fn new(
-        class: &'static str,
-        referent: RBXUUID,
-        name: Name,
-        cframe: CoordinateFrame,
-        b_size: size,
-        color: Color3uint8,
-    ) -> Item {
-        Item {
-            class,
-            referent,
-            properties: vec![
-                // <bool name="Anchored">false</bool>
-                Box::new(Anchored(true)),
-                // <float name="BackParamA">-0.5</float>
-                Box::new(BackParamA(-0.5)),
-                // <float name="BackParamB">0.5</float>
-                Box::new(BackParamB(0.5)),
-                // <token name="BackSurface">0</token>
-                Box::new(BackSurface(0)),
-                // <token name="BackSurfaceInput">0</token>
-                Box::new(BackSurfaceInput(0)),
-                // <float name="BottomParamA">-0.5</float>
-                Box::new(BottomParamA(-0.5)),
-                // <float name="BottomParamB">0.5</float>
-                Box::new(BottomParamB(0.5)),
-                // <token name="BottomSurface">4</token>
-                Box::new(BottomSurface(4)),
-                // <token name="BottomSurfaceInput">0</token>
-                Box::new(BottomSurfaceInput(0)),
-                // <int name="BrickColor">1004</int>
-                Box::new(BrickColor(1004)),
-                // <CoordinateFrame name="CFrame">
-                // 	<X>-14</X>
-                // 	<Y>0.600012004</Y>
-                // 	<Z>23</Z>
-                // 	<R00>1</R00>
-                // 	<R01>0</R01>
-                // 	<R02>0</R02>
-                // 	<R10>0</R10>
-                // 	<R11>1</R11>
-                // 	<R12>0</R12>
-                // 	<R20>0</R20>
-                // 	<R21>0</R21>
-                // 	<R22>1</R22>
-                // </CoordinateFrame>
-                Box::new(cframe),
-                // <bool name="CanCollide">true</bool>
-                Box::new(CanCollide(true)),
-                // <Color3uint8 name="Color3uint8">4294901760</Color3uint8>
-                Box::new(color),
-                // <PhysicalProperties name="CustomPhysicalProperties">
-                // 	<CustomPhysics>false</CustomPhysics>
-                // </PhysicalProperties>
-                Box::new(CustomPhysicalProperties(false)),
-                // <float name="Elasticity">0.5</float>
-                Box::new(Elasticity(0.5)),
-                // <float name="Friction">0.300000012</float>
-                Box::new(Friction(0.5)),
-                // <float name="FrontParamA">-0.5</float>
-                Box::new(FrontParamA(-0.5)),
-                // <float name="FrontParamB">0.5</float>
-                Box::new(FrontParamB(0.5)),
-                // <token name="FrontSurface">0</token>
-                Box::new(FrontSurface(0)),
-                // <token name="FrontSurfaceInput">0</token>
-                Box::new(FrontSurfaceInput(0)),
-                // <float name="LeftParamA">-0.5</float>
-                Box::new(LeftParamA(-0.5)),
-                // <float name="LeftParamB">0.5</float>
-                Box::new(LeftParamB(0.5)),
-                // <token name="LeftSurface">0</token>
-                Box::new(LeftSurface(0)),
-                // <token name="LeftSurfaceInput">0</token>
-                Box::new(LeftSurfaceInput(0)),
-                // <bool name="Locked">false</bool>
-                Box::new(Locked(false)),
-                // <token name="Material">256</token>
-                Box::new(Material(256)),
-                // <string name="Name">Part</string>
-                Box::new(name),
-                // <float name="Reflectance">0</float>
-                Box::new(Reflectance(0.)),
-                // <float name="RightParamA">-0.5</float>
-                Box::new(RightParamA(-0.5)),
-                // <float name="RightParamB">0.5</float>
-                Box::new(RightParamB(0.5)),
-                // <token name="RightSurface">0</token>
-                Box::new(RightSurface(0)),
-                // <token name="RightSurfaceInput">0</token>
-                Box::new(RightSurfaceInput(0)),
-                // <Vector3 name="RotVelocity">
-                // 	<X>0</X>
-                // 	<Y>0</Y>
-                // 	<Z>0</Z>
-                // </Vector3>
-                Box::new(RotVelocity(Vector3 {
-                    x: 0.,
-                    y: 0.,
-                    z: 0.,
-                })),
-                // <float name="TopParamA">-0.5</float>
-                Box::new(TopParamA(-0.5)),
-                // <float name="TopParamB">0.5</float>
-                Box::new(TopParamB(0.5)),
-                // <token name="TopSurface">3</token>
-                Box::new(TopSurface(3)),
-                // <token name="TopSurfaceInput">0</token>
-                Box::new(TopSurfaceInput(0)),
-                // <float name="Transparency">0</float>
-                Box::new(Transparency(0.)),
-                // <Vector3 name="Velocity">
-                // 	<X>0</X>
-                // 	<Y>0</Y>
-                // 	<Z>0</Z>
-                // </Vector3>
-                Box::new(Velocity(Vector3 {
-                    x: 0.,
-                    y: 0.,
-                    z: 0.,
-                })),
-                // <token name="formFactorRaw">1</token>
-                Box::new(formFactorRaw(1)),
-                // <token name="shape">1</token>
-                Box::new(shape(1)),
-                // <Vector3 name="size">
-                // 	<X>4</X>
-                // 	<Y>1.20000005</Y>
-                // 	<Z>2</Z>
-                // </Vector3>
-                Box::new(b_size),
-            ],
-        }
-    }
-}
-
-#[derive(Clone)]
 pub struct Vector3 {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
-macro_rules! vec3_prop {
-    ($i:ident) => {
-        #[allow(non_camel_case_types)]
-        #[derive(Clone)]
-        pub struct $i(pub Vector3);
-
-        impl ToString for $i {
-            fn to_string(&self) -> String {
-                format!(
-                    "<Vector3 name=\"{i}\"><X>{x}</X><Y>{y}</Y><Z>{z}</Z></{i}>",
-                    i = stringify!($i),
-                    x = self.0.x,
-                    y = self.0.y,
-                    z = self.0.z
-                )
-            }
-        }
-    };
-}
-
-macro_rules! simple_property {
-    (string, $name:ident) => {
-        #[allow(non_camel_case_types)]
-        pub struct $name(pub String);
-
-        impl ToString for $name {
-            fn to_string(&self) -> String {
-                format!(
-                    "<string name=\"{n}\">{c}</string>",
-                    c = self.0,
-                    n = stringify!($name)
-                )
-            }
-        }
-    };
-    (int, $name:ident) => {
-        #[allow(non_camel_case_types)]
-        pub struct $name(pub i64);
-
-        impl ToString for $name {
-            fn to_string(&self) -> String {
-                format!(
-                    "<int name=\"{n}\">{c}</int>",
-                    c = self.0,
-                    n = stringify!($name)
-                )
-            }
-        }
-    };
-    (token, $name:ident) => {
-        #[allow(non_camel_case_types)]
-        pub struct $name(pub i64);
-
-        impl ToString for $name {
-            fn to_string(&self) -> String {
-                format!(
-                    "<token name=\"{n}\">{c}</token>",
-                    c = self.0,
-                    n = stringify!($name)
-                )
-            }
-        }
-    };
-    (float, $name:ident) => {
-        #[allow(non_camel_case_types)]
-        pub struct $name(pub f64);
-
-        impl ToString for $name {
-            fn to_string(&self) -> String {
-                format!(
-                    "<float name=\"{n}\">{c}</float>",
-                    c = self.0,
-                    n = stringify!($name)
-                )
-            }
-        }
-    };
-    ($t:ty, $name:ident) => {
-        #[allow(non_camel_case_types)]
-        pub struct $name(pub $t);
-
-        impl ToString for $name {
-            fn to_string(&self) -> String {
-                format!(
-                    "<{t} name=\"{n}\">{c}</{t}>",
-                    t = stringify!($t),
-                    c = self.0,
-                    n = stringify!($name)
-                )
-            }
-        }
-    };
-}
-
-simple_property!(bool, Anchored);
-simple_property!(float, BackParamA);
-simple_property!(float, BackParamB);
-simple_property!(token, BackSurface);
-simple_property!(token, BackSurfaceInput);
-simple_property!(float, BottomParamA);
-simple_property!(float, BottomParamB);
-simple_property!(token, BottomSurface);
-simple_property!(token, BottomSurfaceInput);
-simple_property!(int, BrickColor);
-pub struct CoordinateFrame {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-    pub r00: f64,
-    pub r01: f64,
-    pub r02: f64,
-    pub r10: f64,
-    pub r11: f64,
-    pub r12: f64,
-    pub r20: f64,
-    pub r21: f64,
-    pub r22: f64,
-}
-
-impl ToString for CoordinateFrame {
-    fn to_string(&self) -> String {
-        format!(
-            "<CoordinateFrame name=\"CFrame\">
-    <X>{x}</X>
-    <Y>{y}</Y>
-    <Z>{z}</Z>
-    <R00>{r00}</R00>
-    <R01>{r01}</R01>
-    <R02>{r02}</R02>
-    <R10>{r10}</R10>
-    <R11>{r11}</R11>
-    <R12>{r12}</R12>
-    <R20>{r20}</R20>
-    <R21>{r21}</R21>
-    <R22>{r22}</R22>
-</CoordinateFrame>",
-            x = self.x,
-            y = self.y,
-            z = self.z,
-            r00 = self.r00,
-            r01 = self.r01,
-            r02 = self.r02,
-            r10 = self.r10,
-            r11 = self.r11,
-            r12 = self.r12,
-            r20 = self.r20,
-            r21 = self.r21,
-            r22 = self.r22
-        )
+impl Vector3 {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
+        Vector3 { x, y, z }
     }
 }
-simple_property!(bool, CanCollide);
-pub struct Color3uint8 {
+
+impl ToString for Vector3 {
+    fn to_string(&self) -> String {
+        format!("<X>{}</X><Y>{}</Y><Z>{}</Z>", self.x, self.y, self.z)
+    }
+}
+
+pub struct Color3 {
     pub r: u8,
     pub g: u8,
     pub b: u8,
     pub a: u8,
 }
 
-impl ToString for Color3uint8 {
+impl From<(f32, f32, f32, f32)> for Color3 {
+    fn from((r, g, b, a): (f32, f32, f32, f32)) -> Color3 {
+        println!(
+            "{} {} {} {}",
+            (r * 255.) as u8,
+            (g * 255.) as u8,
+            (b * 255.) as u8,
+            a
+        );
+        let f = |v| (v * 255.) as u8;
+        Color3 {
+            r: f(r),
+            g: f(g),
+            b: f(b),
+            a: f(a),
+        }
+    }
+}
+
+impl ToString for Color3 {
+    fn to_string(&self) -> String {
+        (((self.a as u32) << 24)
+            + ((self.r as u32) << 16)
+            + ((self.g as u32) << 8)
+            + (self.b as u32))
+            .to_string()
+    }
+}
+
+pub struct CFrame {
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
+    pub r00: f32,
+    pub r01: f32,
+    pub r02: f32,
+    pub r10: f32,
+    pub r11: f32,
+    pub r12: f32,
+    pub r20: f32,
+    pub r21: f32,
+    pub r22: f32,
+}
+
+impl ToString for CFrame {
     fn to_string(&self) -> String {
         format!(
-            "<Color3uint8 name=\"Color3uint8\">{}</Color3uint8>",
-            ((self.a as u32) << 24)
-                + ((self.r as u32) << 16)
-                + ((self.g as u32) << 8)
-                + (self.b as u32)
+            "<X>{}</X>
+<Y>{}</Y>
+<Z>{}</Z>
+<R00>{}</R00>
+<R01>{}</R01>
+<R02>{}</R02>
+<R10>{}</R10>
+<R11>{}</R11>
+<R12>{}</R12>
+<R20>{}</R20>
+<R21>{}</R21>
+<R22>{}</R22>",
+            self.x,
+            self.y,
+            self.z,
+            self.r00,
+            self.r01,
+            self.r02,
+            self.r10,
+            self.r11,
+            self.r12,
+            self.r20,
+            self.r21,
+            self.r22
         )
     }
 }
 
-pub struct CustomPhysicalProperties(bool);
+pub struct PhysicalProperties(pub bool);
 
-impl ToString for CustomPhysicalProperties {
+impl ToString for PhysicalProperties {
     fn to_string(&self) -> String {
-        format!("<PhysicalProperties name=\"CustomPhysicalProperties\"><CustomPhysics>{}</CustomPhysics></PhysicalProperties>", self.0)
+        format!("<CustomPhysics>{}</CustomPhysics>", self.0)
     }
 }
 
-// <PhysicalProperties name="CustomPhysicalProperties">
-// 	<CustomPhysics>false</CustomPhysics>
-// </PhysicalProperties>
-simple_property!(float, Elasticity);
-simple_property!(float, Friction);
-simple_property!(float, FrontParamA);
-simple_property!(float, FrontParamB);
-simple_property!(token, FrontSurface);
-simple_property!(token, FrontSurfaceInput);
-simple_property!(float, LeftParamA);
-simple_property!(float, LeftParamB);
-simple_property!(token, LeftSurface);
-simple_property!(token, LeftSurfaceInput);
-simple_property!(bool, Locked);
-simple_property!(token, Material);
-simple_property!(string, Name);
-simple_property!(float, Reflectance);
-simple_property!(float, RightParamA);
-simple_property!(float, RightParamB);
-simple_property!(token, RightSurface);
-simple_property!(token, RightSurfaceInput);
-vec3_prop!(RotVelocity);
-simple_property!(float, TopParamA);
-simple_property!(float, TopParamB);
-simple_property!(token, TopSurface);
-simple_property!(token, TopSurfaceInput);
-simple_property!(float, Transparency);
-vec3_prop!(Velocity);
-simple_property!(token, formFactorRaw);
-simple_property!(token, shape);
-vec3_prop!(size);
+pub struct RbxUuid(pub Uuid);
+
+impl ToString for RbxUuid {
+    fn to_string(&self) -> String {
+        let mut s = "RBX".to_string();
+        s.push_str(&self.0.to_simple_ref().to_string());
+        s
+    }
+}
+
+pub struct Item {
+    pub class: String,
+    pub referent: RbxUuid,
+    pub properties: HashMap<String, Property>,
+}
+
+impl Item {
+    pub fn default(class: String) -> Item {
+        let mut properties: HashMap<String, Property> = HashMap::new();
+        vec![
+            ("Anchored", Property::Bool(true)),
+            ("BackParamA", Property::Float(-0.5)),
+            ("BackParamB", Property::Float(0.5)),
+            ("BackSurface", Property::Token(0)),
+            ("BackSurfaceInput", Property::Token(0)),
+            ("BottomParamA", Property::Float(-0.5)),
+            ("BottomParamB", Property::Float(0.5)),
+            ("BottomSurface", Property::Token(4)),
+            ("BottomSurfaceInput", Property::Token(0)),
+            // ("BrickColor", Property::Int(1004)),
+            ("CanCollide", Property::Bool(true)),
+            (
+                "CustomPhysicalProperties",
+                Property::PhysProps(PhysicalProperties(false)),
+            ),
+            ("Elasticity", Property::Float(0.5)),
+            ("Friction", Property::Float(0.5)),
+            ("FrontParamA", Property::Float(0.5)),
+            ("FrontParamB", Property::Float(0.5)),
+            ("FrontSurface", Property::Token(0)),
+            ("FrontSurfaceInput", Property::Token(0)),
+            ("LeftParamA", Property::Float(0.5)),
+            ("LeftParamB", Property::Float(0.5)),
+            ("LeftSurface", Property::Token(0)),
+            ("LeftSurfaceInput", Property::Token(0)),
+            ("Locked", Property::Bool(false)),
+            ("Material", Property::Token(256)),
+            ("Reflectance", Property::Float(0.)),
+            ("RightParamA", Property::Float(0.5)),
+            ("RightParamB", Property::Float(0.5)),
+            ("RightSurface", Property::Token(0)),
+            ("RightSurfaceInput", Property::Token(0)),
+            ("RotVelocity", Property::Vector3(Vector3::new(0., 0., 0.))),
+            ("TopParamA", Property::Float(-0.5)),
+            ("TopParamB", Property::Float(0.5)),
+            ("TopSurface", Property::Token(3)),
+            ("TopSurfaceInput", Property::Token(0)),
+            ("Transparency", Property::Float(0.)),
+            ("Velocity", Property::Vector3(Vector3::new(0., 0., 0.))),
+            ("formFactorRaw", Property::Token(1)),
+            ("shape", Property::Token(1)),
+        ]
+        .into_iter()
+        .for_each(|(n, p)| {
+            properties.insert(n.to_string(), p);
+        });
+        Item {
+            class,
+            referent: RbxUuid(Uuid::new_v4()),
+            properties,
+        }
+    }
+}
+
+impl ToString for Item {
+    fn to_string(&self) -> String {
+        format!(
+            "<Item class=\"{class}\" referent=\"{referent}\"><Properties>{props}</Properties></Item>",
+            class=self.class,
+            referent=self.referent.to_string(),
+            props=self.properties.iter().map(|(k, v)| v.property_to_string(k)).collect::<Vec<_>>().join("\n")
+        )
+    }
+}
+
+macro_rules! define_property_enum {
+    ($($tag:expr => $t:ident($backing:ty);)*) => {
+        pub enum Property {
+            $($t($backing)),*
+        }
+
+        impl Property {
+            pub fn property_to_string(&self, prop_name: &str) -> String {
+                match self {
+                    $(Property::$t(v) =>
+                        format!(
+                            "<{t} name=\"{n}\">{v}</{t}>",
+                            t = $tag,
+                            n = prop_name,
+                            v = v.to_string()
+                        )
+                    ),*
+                }
+            }
+        }
+    };
+}
+
+define_property_enum! {
+    "bool" => Bool(bool);
+    "float" => Float(f32);
+    "token" => Token(u32);
+    "int" => Int(i64);
+    "CoordinateFrame" => CFrame(CFrame);
+    "Color3uint8" => Color3(Color3);
+    "PhysicalProperties" => PhysProps(PhysicalProperties);
+    "string" => String(String);
+    "Vector3" => Vector3(Vector3);
+}
